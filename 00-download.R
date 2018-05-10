cc0107a94cedc9f58ef98818768b264f556af3a8
Daniel Falbel <dfalbel@gmail.com>
2018-05-10 10:18:58 -0300

    download data

---
+++ 00-download.R
@@ -0,0 +1,8 @@
+# Download and uncompress data
+
+download.file(
+  "https://storage.cloud.google.com/download.tensorflow.org/data/speech_commands_v0.01.tar.gz", 
+  file = "data/speech_commands_v0.01.tar.gz"
+)
+
+untar("speech_commands_v0.01.tar.gz")
