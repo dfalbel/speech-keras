ce464ff60be2f74af10b6743944ce71b8a0fd375
Daniel Falbel <dfalbel@gmail.com>
2018-05-10 10:16:05 -0300

    data import

---
+++ 01-import.R
@@ -0,0 +1,56 @@
+library(stringr)
+library(dplyr)
+
+files <- fs::dir_ls(
+  path = "data/speech_commands_v0.01/", 
+  recursive = TRUE, 
+  glob = "*.wav"
+)
+
+files <- files[!str_detect(files, "background_noise")]
+
+df <- data_frame(
+  fname = files, 
+  class = fname %>% str_extract("1/.*/") %>% 
+    str_replace_all("1/", "") %>%
+    str_replace_all("/", ""),
+  class_id = class %>% as.factor() %>% as.integer() - 1L
+)
+
+library(tfdatasets)
+audio_ops <- tf$contrib$framework$python$ops$audio_ops
+
+ds <- tfdatasets::tensor_slices_dataset(df)
+ds <- ds %>%
+  dataset_map(function(obs) {
+    
+    audio_binary <- tf$read_file(tf$reshape(obs$fname, shape = list()))
+    wav_decoded <- audio_ops$decode_wav(audio_binary, desired_channels = 1)
+    
+    spectrogram <- audio_ops$audio_spectrogram(
+      wav_decoded$audio,
+      window_size = 30,
+      stride = 20,
+      magnitude_squared = TRUE)
+    
+    x <- audio_ops$mfcc(
+      spectrogram,
+      wav_decoded$sample_rate,
+      dct_coefficient_count = 40L
+    )
+    
+    print(x)
+    
+    response <- tf$one_hot(obs$class_id, 29L)
+    list(x, response)
+  }) %>%
+  dataset_shuffle(buffer_size = 100) %>%
+  dataset_repeat() %>%
+  #dataset_batch(32)
+  #dataset_padded_batch(32, list(shape(16000), shape(NULL)))
+
+
+
+batch <- next_batch(ds)
+sess <- tf$Session()
+k <- sess$run(batch)
