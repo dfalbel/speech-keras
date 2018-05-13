506c117ab18af2b32c6bd53855b24daa0c3377ee
Daniel Falbel <dfalbel@gmail.com>
2018-05-13 10:47:03 -0300

    switch link of dataset and add a line to create dir

--- 00-download.R
+++ 00-download.R
@@ -1,8 +1,8 @@
 # Download and uncompress data
 
 download.file(
-  "https://storage.cloud.google.com/download.tensorflow.org/data/speech_commands_v0.01.tar.gz", 
-  file = "data/speech_commands_v0.01.tar.gz"
+  url = "https://storage.cloud.google.com/download.tensorflow.org/data/speech_commands_v0.01.tar.gz", 
+  destfile = "data/speech_commands_v0.01.tar.gz"
 )
 
 untar("speech_commands_v0.01.tar.gz")
